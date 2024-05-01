6da010be00240a2a39f1784399469e854907a333
Sumitisme <scalarscam2004@gmail.com>
2024-05-01 23:01:17 +0545

    Initial design files updated

---
+++ program/test1/test1.ino
@@ -0,0 +1,14 @@
+void setup() {
+    PINMODE(, OUTPUT);
+    PINMODE(, OUTPUT);
+    PINMODE(, OUTPUT);
+    PINMODE(, OUTPUT);
+}
+
+void loop() {
+  // We have:
+  // Row / Col set for ON
+  // Row / Col set for OFF
+  // I will have to try to simulate the circuit in such a manner that the off portion will be handled by itself kind of.
+  
+}
